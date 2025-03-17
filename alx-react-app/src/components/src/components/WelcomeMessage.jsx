2a53544b6f57945beab100de19c3468abe8c27ea
Olalekan Ogundimu <ogundimuolalekan55@gmail.com>
2025-03-17 07:56:01 -0700

    learnt JSX in react

---
+++ alx-react-app/src/components/src/components/WelcomeMessage.jsx
@@ -0,0 +1,14 @@
+function WelcomeMessage ( ){
+    return (
+        <div>
+            <h1>Hello everybody, i am learning react in ALX!</h1>
+            <p>
+                This is a simple JSX component.
+            </p>
+            <p>
+                I am learning about JSX!
+            </p>
+        </div>
+    );
+}
+export default WelcomeMessage;
