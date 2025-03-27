cdc6b8202bdc55c6ed152478cf8f875498c53950
Olalekan Ogundimu <ogundimuolalekan55@gmail.com>
2025-03-27 11:57:00 -0700

    updated

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -6,11 +6,12 @@ const validationSchema = Yup.object({
   email: Yup.string().email("Invalid email").required("Email is required"),
   password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
 });
-// formik form
+// formik 
 
 const FormikForm = () => {
   // formik
   return (
+    // formik
     <Formik
       initialValues={{ username: "", email: "", password: "" }}
       validationSchema={validationSchema}
