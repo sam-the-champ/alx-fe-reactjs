d01a5a4f29ce075a0a52c456be937b8033a5f51e
Olalekan Ogundimu <ogundimuolalekan55@gmail.com>
2025-03-27 05:47:48 -0700

    first commit

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,20 @@
+import { useRecipeStore } from '../store/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.length === 0 ? <p>No recipes yet</p> : null}
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
