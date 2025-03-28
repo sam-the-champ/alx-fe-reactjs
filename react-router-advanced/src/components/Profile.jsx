2330410ab4ddb69f85c962f0b0efe54e68d1e5dc
Olalekan Ogundimu <ogundimuolalekan55@gmail.com>
2025-03-28 03:41:20 -0700

    updated profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,25 @@
-import { Outlet, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
-const Profile = () => (
-  <div>
-    <h1>Profile Page</h1>
-    <nav>
-      <Link to="details">Profile Details</Link> | 
-      <Link to="settings">Profile Settings</Link>
-    </nav>
-    <Outlet />
-  </div>
-);
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default Profile;
